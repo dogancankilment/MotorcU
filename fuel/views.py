7c28b47ddc5dcdd40c55ba0a60b9af3bf8241de2
dogancankilment <dogancankilment@gmail.com>
2018-01-20 19:15:11 +0300

    fuel app created for transition to modular project structure for easy development

---
+++ fuel/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+
+
+def fuel(request):
+    if request.POST:
+        if request.POST["mesafe"] and request.POST["tutar"]:
+            mesafe = request.POST["mesafe"]
+            tutar = request.POST["tutar"]
+
+            tuketim = float(tutar) / float(mesafe)
+
+            c = {"request": request,
+                 "tuketim": tuketim}
+
+            c.update(csrf(request))
+
+            return render_to_response("fuel/index.html", c)
+
+    c = {"request": request}
+    c.update(csrf(request))
+
+    return render_to_response("fuel/index.html", c)
