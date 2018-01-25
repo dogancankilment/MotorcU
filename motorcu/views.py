419916cb5447f75f2adf974674d80e9c60c37eec
dogancankilment <dogancankilment@gmail.com>
2018-01-25 06:31:05 +0300

    home page changed to new dashboard and showing profile login and logout page, and all apps listed in dashboard screen, on the leftside

--- motorcu/views.py
+++ motorcu/views.py
@@ -3,4 +3,13 @@ from django.core.context_processors import csrf
 
 
 def home(request):
-    return render_to_response("home/index.html")
+    c = {"request": request}
+    c.update(csrf(request))
+    return render_to_response("home/new-dashboard.html", c)
+
+
+# def test_new_dashboard(request):
+#     c = {"request": request}
+#     c.update(csrf(request))
+#
+#     return render_to_response("home/new-dashboard.html", c)
